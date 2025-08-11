a1199bb4a96fbb9093974ec1b818795eafc6c1f9
i4kdev <andrei.iakimov@outlook.com>
2025-08-11 16:20:17 -0400

    feat: Banner added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import NavComponent from './components/NavComponent'
+import Banner from './components/Banner'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <NavComponent />
+      <Banner />
     </>
   )
 }
